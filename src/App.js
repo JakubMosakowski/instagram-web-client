aabe43d8e15bc26d25c4dd93b9ce729df464eb33
jakubmosakowski <jmosakowski@spoton.com>
2023-11-04 20:35:20 +0100

    Add initial page implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import moment from 'moment';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [stories, setStories] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const dateParam = moment().format('DD_MM_YYYY');
+            const response = await fetch(`/api/stories?date=${dateParam}`);
+            const data = await response.json();
+            setStories(data.urls);
+        };
+
+        fetchData().catch(console.error);
+    }, []);
+
+    // A helper function to determine if a URL is for a video
+    const isVideo = (url) => {
+        return /\.(mp4|webm)(\?.*)?$/.test(url);
+    };
+
+    return (
+        <div>
+            {stories.map((url) => (
+                <div key={url}>
+                    {isVideo(url) ? (
+                        <video controls src={url}/>
+                    ) : (
+                        <img src={url} alt={`Story ${url}`}/>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
